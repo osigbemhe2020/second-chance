fe718b44dc327430b44a242aad6aff81d71d01ae
osigbemhe2020 <62507389+osigbemhe2020@users.noreply.github.com>
2025-08-02 15:48:18 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,20 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
+    node: true,
     es2021: true
   },
-  extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
+  extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 12
   },
   rules: {
+    semi: ['warn', 'always'], // or 'off'
+    quotes: ['warn', 'single'],
+    'no-unused-vars': ['warn'],
+    indent: ['warn', 2],
+    'comma-spacing': ['warn', { before: false, after: true }],
+    'object-curly-spacing': ['warn', 'always'],
+    'no-multiple-empty-lines': ['warn', { max: 1 }]
   }
-}
+};
+
